a3be811ff22c689d34d57a06188a1e78fa23d3bc
YousufSH <160315514+ShaikhYousuf39@users.noreply.github.com>
2024-05-06 21:59:31 +0500

    Add files via upload

---
+++ new.js
@@ -0,0 +1,11 @@
+import inquirer from "inquirer";
+const ans = await inquirer.prompt([
+    {
+        type: "input",
+        name: "sent",
+        message: "Enter the sentence : "
+    }
+]);
+const complete = ans.sent.trim().split(" ");
+console.log(complete);
+console.log(`Your Sentence Have ${complete.length} words.`);
